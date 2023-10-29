420e96106b2259a95994e9b907c410e539c4a29e
Mihael <nosil.mihael@gmail.com>
2023-10-29 15:54:05 +0100

    back to basic

--- labos1/server.js
+++ labos1/server.js
@@ -1,26 +1,17 @@
 const express = require('express');
-const { auth } = require('express-openid-connect')
+const { auth } = require('./auth0.js');
 require('dotenv/config');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port=3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 ;
 
-const config = {
-  authRequired: false,
-  auth0Logout: true,
-  baseURL: process.env.BASE_URL,
-  clientID: process.env.CLIENT_ID,
-  issuerBaseURL: process.env.ISSUER_BASE_URL,
-  secret: process.env.SECRET
-};
+app.get('/api/message', (req, res) => {
+  res.json({ message: 'Hello from the server!' });
+});
 
-// auth router attaches /login, /logout, and /callback routes to the baseURL
-app.use(auth(config));
-
-// req.isAuthenticated is provided from the auth router
-app.get('/', (req, res) => {
-  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out')
+app.listen(port, () => {
+  console.log(`Express server running at http://localhost:${port}`);
 });
